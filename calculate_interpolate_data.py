dd6562eba43a46cbf1a2bcc9e0c7fd3d53aa5919
tmhuysen <tobias.huysentruyt@minimoa.net>
2019-03-25 16:40:39 +0100

    drivers

--- calculate_interpolate_data.py
+++ calculate_interpolate_data.py
@@ -1,13 +1,23 @@
 import numpy as np
-
+import os
+import sys
 from qollib.DataSet import ConstrainedData
 from qollib.executable import Executable1D, ConstrainedExeWrapper
 from qollib.graphing import ConstrainedGrapher
 from qollib.interpolation import IterativeInterpolator
 
-#path = "/Users/quantum/CLionProjects/forks/gqcp/build/drivers/c_constrained_N_O"
-path = "/Users/quantum/PycharmProjects/QOL/personalgen/exes/c_constrained_N_O"
-exe = Executable1D(path, ["-d", "-s", "-n"], ["10.0", "STO-3G", "true"], "-c", True, name = "naturals")
+
+
+exelist = ["constrained_N_O", "c_constrained_N_O"]
+
+distance = sys.argv[1]
+exe = int(sys.argv[2])
+print(exe)
+
+pathx = os.environ['PEXE1']
+path = os.path.join(pathx, exelist[exe])
+
+exe = Executable1D(path, ["-d", "-s"], [str(distance), "STO-3G"], "-c", True, name = "linux")
 
 constrained_exe = ConstrainedExeWrapper(exe)
 
@@ -18,4 +28,4 @@ x = np.arange(minb, maxb, 0.01)
 modulels = IterativeInterpolator(maxb, minb, constrained_exe, ConstrainedData.get_population,
                                  ConstrainedData.get_lambda)
 
-modulels.interpolateIteratively(0.01, 0.01, 5, x)
+modulels.interpolateIteratively(0.01, 0.01, 10, x)
---
+++ qollib/Drivers/calculate_interpolate_data.py
@@ -0,0 +1,31 @@
+import numpy as np
+import os
+import sys
+from qollib.DataSet import ConstrainedData
+from qollib.executable import Executable1D, ConstrainedExeWrapper
+from qollib.graphing import ConstrainedGrapher
+from qollib.interpolation import IterativeInterpolator
+
+
+
+exelist = ["constrained_N_O", "c_constrained_N_O"]
+
+distance = sys.argv[1]
+exe = int(sys.argv[2])
+print(exe)
+
+pathx = os.environ['PEXE1']
+path = os.path.join(pathx, exelist[exe])
+
+exe = Executable1D(path, ["-d", "-s"], [str(distance), "STO-3G"], "-c", True, name = "linux")
+
+constrained_exe = ConstrainedExeWrapper(exe)
+
+minb = -5.0
+maxb = 5.0
+x = np.arange(minb, maxb, 0.01)
+
+modulels = IterativeInterpolator(maxb, minb, constrained_exe, ConstrainedData.get_population,
+                                 ConstrainedData.get_lambda)
+
+modulels.interpolateIteratively(0.01, 0.01, 10, x)
